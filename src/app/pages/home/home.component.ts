c9ba2d863c1eae8ef9ef34780146682c15b90a85
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-04 22:01:23 -0300

    fix: adiciona loading e tempo de exibição na página

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LoadingComponent } from '../../components/loading/loading/loading.component';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-home',
-  imports: [NavbarComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+	selector: 'app-home',
+	imports: [NavbarComponent, CommonModule, LoadingComponent],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+	loading = true;
+
+	ngOnInit(): void {
+		setTimeout(() => {
+			this.loading = false;
+		}, 1000);
+	}
 }
