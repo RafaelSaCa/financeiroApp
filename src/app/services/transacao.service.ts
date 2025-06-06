c4fa6843916a45260e1e4aa701f35f5cde1d4c06
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-05 22:19:30 -0300

    feat: Serviço de transação

---
+++ src/app/services/transacao.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Transacoes } from '../models/transacoes';
+import { Transacao } from '../models/transacao';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class TransacaoService {
+
+	private apiUrl = "http://localhost:8080/transacoes";
+
+	constructor(private http: HttpClient) { }
+
+	getAll(): Observable<Transacoes> {
+		return this.http.get<Transacoes>(this.apiUrl);
+	}
+
+	create(transacao: Transacao): Observable<Transacao> {
+		return this.http.post<Transacao>(this.apiUrl, transacao);
+	}
+
+	edit(id: number, transacao: Transacao): Observable<Transacao> {
+		return this.http.put<Transacao>(`${this.apiUrl}/${id}`, transacao);
+	}
+
+	delete(id: number): Observable<void> {
+		return this.http.delete<void>(`${this.apiUrl}/${id}`);
+	}
+}
