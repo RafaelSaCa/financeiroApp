48bbdfb4ed942801e6e5211f2be2a264000ab986
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-08 22:29:27 -0300

    feat: adiciona NgToast para alertas e icones fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router, RouterOutlet, Event as RouterEvent } from '@angular/router';
+import { filter } from 'rxjs';
+import { LoadingComponent } from './components/loading/loading/loading.component';
+import { NgToastComponent, NgToastService, TOAST_POSITIONS } from 'ng-angular-popup';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [RouterOutlet, CommonModule],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'financeiroApp';
+	title = 'financeiroApp';
+
 }
+
+
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,19 +1,32 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { routes } from './app.routes';
 import { AuthInterceptor } from './auth/interceptor/auth.interceptor';
+import { provideNgToast } from 'ng-angular-popup';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-      provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptorsFromDi()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    }
+    },
+
+    BrowserAnimationsModule,
+    provideAnimationsAsync(),
+    provideAnimations(),
+    provideNgToast(),
+
+
   ]
 };
+
+
+
