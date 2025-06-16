07fcd2c507710dc44c9d8273bcf1db02bcb642f2
RafaelSaCa <raffaellsaca@gmail.com>
2025-06-15 22:21:21 -0300

    feat: rota para gerenciamento de categorias

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthGuard } from './auth/guard/auth.guard';
 import { HomeComponent } from './pages/home/home.component';
 import { RelatoriosComponent } from './pages/relatorios/relatorios.component';
+import { CadastrosComponent } from './pages/cadastros/cadastros.component';
 
 export const routes: Routes = [
     {
@@ -20,6 +21,12 @@ export const routes: Routes = [
     {
         path: 'relatorios',
         component: RelatoriosComponent,
+        canActivate: [AuthGuard]
+    },
+
+      {
+        path: 'gerenciamento',
+        component: CadastrosComponent,
         canActivate: [AuthGuard]
     }
 
